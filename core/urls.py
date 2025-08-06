982aa4d630f8dd3dfff35b603503f86795c14110
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2025-08-06 20:19:28 +0300

    creating movie detail and added to path

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from . import views
 app_name = 'core'
 urlpatterns = [
     path('movies', views.MovieListView.as_view(), name='MovieList'),
+    path('movies/<int:pk>/', views.MovieDetailView.as_view(), name='MovieDetail'),  
     # Add more URL patterns as needed
 ]
