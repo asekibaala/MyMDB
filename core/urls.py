ca6a0d406d1212f49bf66805cd5af7e2c69dcce1
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2025-08-10 18:20:35 +0300

    fix bug for movie detail

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+
 app_name = 'core'
+
 urlpatterns = [
     path('movies', views.MovieListView.as_view(), name='MovieList'),
-    path('movies/<int:pk>/', views.MovieDetailView.as_view(), name='MovieDetail'),  
-    # Add more URL patterns as needed
+    path('movies/<int:pk>/', views.MovieDetailView.as_view(), name='MovieDetail'),
+    path('movie/<int:pk>/', views.MovieDetailView.as_view()),  # <-- Add this line
 ]
