23dfe1a00ab2198fa24bd5661d7c5cd9862e7326
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2025-08-12 21:45:38 +0300

    adding 404 custom page

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('movies', views.MovieListView.as_view(), name='MovieList'),
     path('movies/<int:pk>/', views.MovieDetailView.as_view(), name='MovieDetail'),
     path('movie/<int:pk>/', views.MovieDetailView.as_view()),  # <-- Add this line
+    path('404/', views.Custom404View.as_view(), name='Custom404'),
 ]
