57acd09f517df7bce40b9b72a6c791f1e448d603
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2025-08-12 21:48:19 +0300

    change debug to false

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r_2adfohiiwwu03e&k+jx^x0zs=_#dyg&)fyps!xw@c1+ot&q%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['*']
