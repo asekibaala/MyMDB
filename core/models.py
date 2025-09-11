07e3a19a11250ab19933c28c6a2263e7c5b054df
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2025-09-11 05:37:36 +0300

    fix import bug in models file

--- core/models.py
+++ core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
+from django.conf import settings
 
-from config import settings
 #from core.models import MovieManager
 class PersonManager(models.Manager):
     def all_with_prefetch_movies(self):
