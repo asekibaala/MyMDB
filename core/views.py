62f1337f705c55f7c2d935957a656d75ba270305
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2025-09-16 05:59:44 +0300

    updating movie detail

--- core/views.py
+++ core/views.py
@@ -1,11 +1,28 @@
 from django.views.generic import (ListView, DetailView)
-from core.models import Movie,Person
+from networkx import reverse
+from core.models import Movie,Person, Vote,VoteManager
 
 
 class MovieDetailView(DetailView):
     #model = Movie
     queryset = Movie.objects.all_with_related_persons()
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            vote = Vote.objects.get_vote_or_unsaved_blank_vote(
+                movie=self.object,
+                user=self.request.user
+            )
+            if vote.id:
+                vote_form_url = reverse('core:UpdateVote', kwargs={'movie_id': self.object.id, 'pk': vote.id})
+            else:
+                vote_form_url = reverse('core:CreateVote',kwargs={'movie_id':self.object.id})
+            vote_form = VoteForm(instance=vote)
+            context['vote_form'] = vote_form
+            context['vote_form_url'] = vote_form_url
+        return context
+    
 class MovieListView(ListView):
     model = Movie
     paginate_by = 10
