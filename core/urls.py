e269ecc1252d5511842fb07f5329c620e319ad8c
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2025-10-04 18:38:13 +0300

    New URL pattern for image upload

--- core/urls.py
+++ core/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('movies/<int:pk>/', views.MovieDetailView.as_view(), name='MovieDetail'),
     path('movie/<int:pk>/', views.MovieDetailView.as_view()),  # <-- Add this line
     #path('404/', views.Custom404View.as_view(), name='Custom404'),
+    path('movie/<int:movie_id>/image/upload', views.MovieImageUploadView.as_view(), name='MovieImageUpload'),  # New URL pattern for image upload
 ]
